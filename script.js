b8e0f2966cf22f8bc8330c5b22576ac0f69c9c05
Mohamedhajaj511 <jinalk620@gmail.com>
2025-02-13 00:43:01 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello!');
+const images = ["product1.jpg", "product2.jpg", "product3.jpg"];
+let currentIndex = 0;
+
+document.getElementById("prev").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+});
+
+document.getElementById("next").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+});
+
+function updateImage() {
+    document.getElementById("productImage").src = images[currentIndex];
+}
+
+function changeImage(imageSrc) {
+    document.getElementById("productImage").src = imageSrc;
+}
